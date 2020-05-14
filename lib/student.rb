9155d14b2ad1e22669d1e517394bd3d2dda0f38f
Stephen McBride <stevemcbride3@gmail.com>
2020-05-14 19:24:47 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,48 @@
+require "pry"
 class Student
   attr_accessor :id, :name, :grade
 
+  @@all = nil
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    @@all = DB[:conn].execute("SELECT * FROM students").map {|row| new_from_db(row)}
+  end
+
+  def self.all_students_in_grade_9
+    self.all.select do |student|
+      student.grade == "9"
+    end
+  end
+
+  def self.students_below_12th_grade
+    self.all.select {|student| student.grade < "12"}
+  end
+
+  def self.first_X_students_in_grade_10(limit)
+    students = self.all.select {|student| student.grade == "10"}
+    students[0..limit - 1]
+  end
+
+  def self.first_student_in_grade_10
+    self.all.find {|student| student.grade == "10"}
+  end
+
+  def self.all_students_in_grade_X(grade)
+    self.all.select {|student| student.grade == grade.to_s}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT * FROM students
+          WHERE name == ?"
+    new_from_db(DB[:conn].execute(sql, name)[0])
   end
   
   def save
